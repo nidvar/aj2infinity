21923ee6d438b4acae55db7140a7faf4f5d45dc6
Jay Kim <djayshine87@hotmail.com>
2020-08-08 16:20:28 +1000

    added infinite scrolling. need to calibrate it

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log('infinite scroll');
 
 const KEY = `Qzhjls8Ie8Ci683ZM0dsCA-VdVPT1_bjsiwRRkLAPWU`
-const count = 10
-const URL = `https://api.unsplash.com/photos/random/?client_id=${KEY}&count=${30}`
+const count = 5
+const URL = `https://api.unsplash.com/photos/random/?client_id=${KEY}&count=${count}`
 
 const generate_pictures=(url)=>{
     const pic = document.createElement('img');
@@ -10,7 +10,7 @@ const generate_pictures=(url)=>{
     document.getElementById('output').appendChild(pic)
 }
 
-const grab_photos = async ()=>{
+const grab_photos = async (number)=>{
     const response = await fetch(URL)
     const data = await response.json();
     console.log(data)
@@ -20,9 +20,12 @@ const grab_photos = async ()=>{
     })
 }
 
-
-
-
-
+window.addEventListener('scroll',()=>{
+    console.log(document.body.offsetHeight, window.scrollY)
+    if(document.body.offsetHeight - window.scrollY < 1200){
+        console.log('time to load again')
+        grab_photos();
+    }
+})
 
 grab_photos();
